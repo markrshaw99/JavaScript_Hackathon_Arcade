73cb75399fb3ca0fa1705d7269644d0fb4155644
AbraWalker <abraxaswalker@hotmail.com>
2025-06-23 00:26:59 +0100

    adjust js and css to make spikes "disappear" when they leave the game screen

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,14 +6,21 @@ let gravity = 0.9;
 let isJumping = false;
 let isGameOver = false;
 
+//controls handler
 function control(event) {
-    if (event.code === 'Space' || event.key === ' ') {
-        if (!isJumping) {
-            jump();
-        }  
+    //ensure player cant jump if game is over
+    if (!isGameOver) {
+        if (event.code === 'Space' || event.key === ' ') {
+            if (!isJumping) {
+                jump();
+            }
+        }
     }
 }
 
+document.addEventListener('keydown', control);
+
+//main functionality
 function jump() {
     isJumping = true;
     let jumpCount = 0;
@@ -41,28 +48,37 @@ function jump() {
     }, 20)
 }
 
+//obstacle generation + game over state
 function obstacles() {
-    let spikePos = 480;
-    const spike = document.createElement('div');
-    spike.classList.add('spike');
-    grid.appendChild(spike);
-    spike.style.left = spikePos + 'px';
 
-    let timer = setInterval(function () {
-        if (spikePos < 5 /*5 to account for border/box-shadow*/) {
-            clearInterval(timer);
-            alert.innerHTML = '!GAME OVER!';
-            isGameOver = true;
-            //remove all children from grid, while grid has children, remove last child
-            while (grid.firstChild) {
-                grid.removeChild(grid.last);
-            }
-        }
-        spikePos -= 10;
+    if (!isGameOver) {
+        let randomTime = Math.random() * 4000;
+        let spikePos = 480;
+        const spike = document.createElement('div');
+        spike.classList.add('spike');
+        grid.appendChild(spike);
         spike.style.left = spikePos + 'px';
-    }, 20)
+
+        let timer = setInterval(function () {
+            if (spikePos > 5 && spikePos < 60 && position < 60) {
+                clearInterval(timer);
+                alert.innerHTML = '!GAME OVER!';
+                isGameOver = true;
+                //remove all children from grid, while grid has children, remove last child
+                while (grid.firstChild) {
+                    grid.removeChild(grid.last);
+                }
+            }
+            spikePos -= 10;
+            spike.style.left = spikePos + 'px';
+            if (spikePos < -40) {
+                spike.style.border = 'none';
+            }
+        }, 20)
+
+        setTimeout(obstacles, 4000);
+    }
+
 }
 
 obstacles();
-
-document.addEventListener('keydown', control);
