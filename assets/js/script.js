08ca536efcdfca2e5c2573f6edaa8c8685c7f975
AbraWalker <abraxaswalker@hotmail.com>
2025-06-23 12:51:34 +0100

    add score functionality, fix deletion bug, add game state

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,15 @@
-const chara = document.querySelector('.character');
+
 const grid = document.querySelector('.grid');
 const alert = document.getElementById('alert');
+const scoreText = document.getElementById('score-display');
+const highScoreText = document.getElementById('high-score-display');
+
 let position = 0;
 let gravity = 0.9;
 let isJumping = false;
 let isGameOver = false;
+let score = 0;
+let highScore = 0;
 
 //controls handler
 function control(event) {
@@ -16,12 +21,36 @@ function control(event) {
             }
         }
     }
+    else if (isGameOver) {
+        if (event.code === 'Space' || event.key === ' ') {
+            startGame();
+        }
+    }
 }
 
 document.addEventListener('keydown', control);
 
+//when game restarts, delete the last obstacle from the previous game, remove alert, reset game state
+function startGame() {
+    score = 0;
+    isGameOver = false;
+    alert.innerHTML = ' ';
+    cleanUp();
+    let char = getCharacter();
+    if (!char) {
+        const character = document.createElement('div');
+        character.classList.add('character');
+        grid.appendChild(character);
+    }
+}
+
+function getCharacter() {
+    return document.querySelector('.character');
+}
+
 //main functionality
 function jump() {
+    let chara = getCharacter();
     isJumping = true;
     let jumpCount = 0;
     let timer = setInterval(function () {
@@ -33,7 +62,7 @@ function jump() {
                     clearInterval(downTimer);
                     isJumping = false;
                 }
-                position -= 5; 
+                position -= 5;
                 jumpCount--;
                 position *= gravity;
                 chara.style.bottom = position + 'px';
@@ -54,6 +83,7 @@ function obstacles() {
     if (!isGameOver) {
         let randomTime = Math.random() * 4000;
         let spikePos = 480;
+
         const spike = document.createElement('div');
         spike.classList.add('spike');
         grid.appendChild(spike);
@@ -64,10 +94,6 @@ function obstacles() {
                 clearInterval(timer);
                 alert.innerHTML = '!GAME OVER!';
                 isGameOver = true;
-                //remove all children from grid, while grid has children, remove last child
-                while (grid.firstChild) {
-                    grid.removeChild(grid.last);
-                }
             }
             spikePos -= 10;
             spike.style.left = spikePos + 'px';
@@ -81,4 +107,28 @@ function obstacles() {
 
 }
 
+var getScores = setInterval(function () {
+    let currentScore = 0;
+    currentScore = Math.floor(score / 100);
+
+    if (!isGameOver) {
+        score++;
+        scoreText.innerHTML = currentScore;
+    } else {
+        if (currentScore > highScore) {
+            highScore = currentScore;
+            highScoreText.innerHTML = highScore;
+            console.log(highScore);
+        }
+    }
+}, 10);
+
+function cleanUp() {
+    //remove all children from grid, while grid has children, remove last child
+    while (grid.firstChild) {
+        grid.removeChild(grid.lastChild);
+    }
+}
+
+//call to generate obstacles
 obstacles();
